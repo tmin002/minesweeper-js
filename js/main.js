34e1f792d355acee6d00c2684076c7869e4d187f
tmin002 <ssh9930@gmail.com>
2023-06-25 19:39:35 +0900

    test number display

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ import {bindCanvasEvents} from './canvas.js';
 import {getCanvasContext} from './canvas.js';
 
 // minesweeper board map
-const mineBoard = new Board(500);
+const mineBoard = new Board(50);
 
 window.onload = () => {
 	render();
@@ -14,7 +14,16 @@ window.onload = () => {
 	let x = getCanvasContext();
 	for (let i=0; i<40; i++) {
 		for (let j=0; j<40; j++) {
+			let block = mineBoard.map[i][j];
 
+			if (block.isMine) {
+				x.fillStyle = 'rgb(255,0,0)';
+				x.fillText('m', i*20, j*20); 
+				continue;
+			}
+
+			x.fillStyle = 'rgb(0,255,0)';
+			x.fillText(String(block.nearCount), i*20, j*20); 
 		}
 	}
 
